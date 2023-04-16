7dbf6f9158419572818ed2b75d91f3153dd9992a
rekavasarhelyinagy <vnreka2000@gmail.com>
2023-04-16 12:33:13 +0200

    player1 movable

--- script.js
+++ script.js
@@ -3,7 +3,10 @@ function setup() {
 }
 
 //seed character
-function player1() {
+function player1(player1X, player1Y) {
+  push();
+  translate(player1X, player1Y);
+
   //head
   noStroke();
   fill(198, 136, 66);
@@ -20,6 +23,8 @@ function player1() {
   ellipse(208, 240, 5, 6);
   fill(0, 0, 0);
   ellipse(208, 240, 3);
+
+  pop();
 }
 
 //water character
@@ -62,15 +67,18 @@ function player2(player2X, player2Y) {
 //VARIABLES
 let y = 0;
 let snows = [];
+//player1
+let player1X = -150;
+let player1Y = -150;
 //player2
 let player2X = 100;
 let player2Y = 100;
 
 function draw() {
-  player1();
+  player1(player1X, player1Y);
   player2(player2X, player2Y);
-//   for (let snow of snows) {
-//     drawSnow(snow);
-//     updateSnow(snow);
-//   }
+  //   for (let snow of snows) {
+  //     drawSnow(snow);
+  //     updateSnow(snow);
+  //   }
 }
