f7d878a39797fa624dc825bb6f2587ba8a899cdb
polidor01 <polinadoratis@gmail.com>
2023-05-17 22:38:55 +0200

    shooting part

--- script2.js
+++ script2.js
@@ -7,19 +7,28 @@ let pSpeed = 3;
 
 //player
 let p2X = 50;
-let p2Y = 300;
+let p2Y = 300; 
 let p2Width = 50;
 let p2Height = 50;
 let p2Speed = 3;
 
 //seed
-let seed1X = p1X + p2X;
-let seed1Y = p1Y + p2Y;
+let seed1X = p1X;
+let seed1Y = p1Y;
 let seed1Position = 0; //keeping track of where the seed is at the moment
 let seedWidth = 18;
 let seedHeight = 10;
 let seedSpeed = 5;
 let fire = false; //am i firing the seed?
+
+//water droplets
+let waterX = p2X;
+let waterY = p2Y;
+let waterPosition = 0; //keeping track of where the seed is at the moment
+let waterWidth = 15;
+let waterHeight = 20;
+let waterSpeed = 5;
+let fire2 = false; //am i firing the seed?
 
 
 
@@ -37,13 +46,32 @@ let hole1Y = 180;
 let holeWidth = 40;
 let holeHeight = 40;
 
+
+//hole with water droplets
+let water2B = 500;
+let water2C = 180;
+let water2Width = 40;
+let water2Height = 40;
+
+
+
 let groundArray = [{
     x: 450, y: 180, hit: false 
 }, {x: 300, y: 100, hit: false
 }, {x: 280, y: 220, hit: false
 }, {x: 320, y: 355, hit: false
 }, {x: 340, y: 280, hit: false
-}, {x: 430, y: 30, hit: false
+}, {x: 430, y: 30,  hit: false
+}
+];
+
+let waterGroundArray = [{
+  x: 450, y: 180, hit: false 
+}, {x: 300, y: 100, hit: false
+}, {x: 280, y: 220, hit: false
+}, {x: 320, y: 355, hit: false
+}, {x: 340, y: 280, hit: false
+}, {x: 430, y: 30,  hit: false
 }
 ];
 
@@ -56,9 +84,9 @@ function setup() {
 
 function draw(){
     //call looping functions
-    keyPressed();
+    keyPressedChecked();
     keyTyped();
-     
+      
 
     background(255, 255, 255);
 
@@ -75,22 +103,42 @@ function draw(){
 
 
     //activate the seed function
-    seeds(); 
-
+    seeds();
+    water(); 
 
     //collisions between seed and ground gap
+
     for (let i = 0; i < 6; i++) {
-        noStroke();
-        fill(0, 0, 0);
-        ellipse(groundArray[i].x, groundArray[i].y, gHeight, gWidth);
-        if (dist(seed1X, seed1Y, groundArray[i].x, groundArray[i].y) <= 40) {
-            groundArray[i].hit = true;
-            seed1Position = 2;
-        }      
-        if (groundArray[i].hit) {
-           hole(groundArray[i].x, groundArray[i].y);
-        } 
-    }
+      noStroke();
+      fill(0, 0, 0);
+      ellipse(groundArray[i].x, groundArray[i].y, gHeight, gWidth);
+      if (dist(seed1X, seed1Y, groundArray[i].x, groundArray[i].y) <= 40) {
+          groundArray[i].hit = true;
+          seed1Position = 2;
+      }
+      if (groundArray[i].hit) {
+          hole(groundArray[i].x, groundArray[i].y);
+      }
+  }
+  
+  for (let i = 0; i < 6; i++) {
+      noStroke();
+      fill(0, 0, 0);
+      ellipse(waterGroundArray[i].x, waterGroundArray[i].y, gHeight, gWidth);
+      if (dist(waterX, waterY, waterGroundArray[i].x, waterGroundArray[i].y) <= 40) {
+          waterGroundArray[i].hit = true;
+          waterPosition = 3;
+      }
+  }
+  
+  for (let i = 0; i < 6; i++) {
+      if (groundArray[i].hit) {
+          hole(groundArray[i].x, groundArray[i].y);
+      }
+      if (waterGroundArray[i].hit) {
+          waterHole(waterGroundArray[i].x, waterGroundArray[i].y);
+      }
+  }
 
 }
 
@@ -103,6 +151,15 @@ ellipse(x, y, holeHeight, holeWidth);
 fill(128, 36, 66);
 ellipse(x, y, 18, 10);
 } 
+
+function waterHole(b, c){
+  //draw hole with waterdroplets
+  noStroke();
+  fill(0, 0, 0);
+  ellipse(b, c, water2Height, water2Width);
+  fill(79, 211, 243);
+  ellipse(b, c, 20);
+  } 
 
 
 function seeds(){
@@ -149,14 +206,58 @@ ellipse(seed1X, seed1Y, seedWidth, seedHeight);
 }
 
 
-//make the player1 move up and down
-function keyPressed(){
+function water(){
+  //seed positions
+      //0 = with player1 ready to be fired
+      //1 = in motion after firing
+      //3 = collision with object, return to p2
+      
+  
+  //draw seed   
+  noStroke();
+  fill(79, 211, 243);
+  ellipse(waterX, waterY, waterWidth, waterHeight);  
+  
+  //keep track and fire the seeds
+    if(fire2 === true && waterPosition === 0){
+      waterPosition = 1;
+    }
+  //fire seeds code
+    if(waterPosition === 1){
+      waterY = waterY; //stops following p1
+      waterX = waterX + waterSpeed; //moves horizontally
+  
+      //if it misses
+      if(waterX >= 500){
+        waterPosition = 3; //reload
+      }
+  
+    }
+    else{
+      //when you are not firing, the seed should be with p1
+      waterY = p2Y;
+      waterX = p2X;
+      
+    }
+  
+  //reload on #2 command
+    if(waterPosition === 3){
+      waterY = p2Y;
+      waterX = p2X;
+      waterPosition = 0;//reset so you can fire again
+    } 
     
-    if(keyCode === UP_ARROW && keyIsPressed){ //player1 moving only when the key is pressed
+  }
+
+
+//make the player1 and player2 move up and down
+function keyPressedChecked(){
+    
+    if(keyIsDown(UP_ARROW)){ //player1 moving only when the key is pressed
         p1Y = p1Y - pSpeed;
     }
 
-    if(keyCode === DOWN_ARROW && keyIsPressed){ //player1 moving only when the key is pressed
+    if(keyIsDown(DOWN_ARROW)){ //player1 moving only when the key is pressed
         p1Y = p1Y + pSpeed;
     } 
 
@@ -179,11 +280,13 @@ function keyTyped(){
         fire = false;
     }
 
-    if(key === 'e' && keyIsPressed){
-        fire = true; //seed will be fired on key press
+    if(key === 'e' && keyIsPressed) {
+
+      console.log("shoot");
+        fire2 = true; //water will be fired on key press
     }
     else{
-        fire = false;
+        fire2 = false;
     }
 }
 
